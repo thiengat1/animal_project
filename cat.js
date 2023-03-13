512b01174858ac23cd5a8d43755ae24cef29ced3
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-13 23:53:25 +0800

    fix missing export

---
+++ cat.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: Lewis
+ * @Date: 2023-03-13 23:34:08
+ * @LastEditTime: 2023-03-13 23:50:41
+ * @LastEditors: Lewis
+ */
+function cat() {}
+
+module.export = cat;
--- mouse.js
+++ mouse.js
@@ -2,9 +2,11 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2023-03-13 23:47:42
- * @LastEditTime: 2023-03-13 23:48:06
+ * @LastEditTime: 2023-03-13 23:49:32
  * @LastEditors: Lewis
  */
 function mouse(name) {
   this.name - name;
 }
+
+module.export = mouse;
