5e8d00f6f8bf2e52f8c3b1b55abdc63a04bbaa9f
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-14 00:05:13 +0800

    add die method

--- mouse.js
+++ mouse.js
@@ -2,11 +2,16 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2023-03-13 23:47:42
- * @LastEditTime: 2023-03-13 23:49:32
+ * @LastEditTime: 2023-03-13 23:59:02
  * @LastEditors: Lewis
  */
 function mouse(name) {
   this.name - name;
+  this.dead = false;
 }
+
+mouse.prototype.die = function () {
+  this.dead = true;
+};
 
 module.export = mouse;
