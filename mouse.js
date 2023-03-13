2881619b68d8bf0a36458ccce6963215299a5098
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-13 23:49:08 +0800

    add mouse module

---
+++ mouse.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: Lewis
+ * @Date: 2023-03-13 23:47:42
+ * @LastEditTime: 2023-03-13 23:48:06
+ * @LastEditors: Lewis
+ */
+function mouse(name) {
+  this.name - name;
+}
