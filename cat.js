104475694d0cdaed0e9f5bd0fefbcc8647549aed
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-14 00:35:38 +0800

    eat method for cat

--- cat.js
+++ cat.js
@@ -2,11 +2,13 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2023-03-13 23:34:08
- * @LastEditTime: 2023-03-13 23:58:18
+ * @LastEditTime: 2023-03-14 00:12:04
  * @LastEditors: Lewis
  */
 function cat() {
   this.stomach = [];
 }
-
+cat.prototype.eat = function (mouse) {
+  this.stomach.push(mouse);
+};
 module.export = cat;
