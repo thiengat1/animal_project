c06d2cc98f17c335b5ceb924253be65f39577aa3
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-14 00:06:24 +0800

    add stomach property

--- cat.js
+++ cat.js
@@ -2,9 +2,11 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2023-03-13 23:34:08
- * @LastEditTime: 2023-03-13 23:50:41
+ * @LastEditTime: 2023-03-13 23:58:18
  * @LastEditors: Lewis
  */
-function cat() {}
+function cat() {
+  this.stomach = [];
+}
 
 module.export = cat;
