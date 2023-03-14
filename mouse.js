28ffa71f42fe3182ca0a54148d30ec106ebac0a4
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-15 00:29:31 +0800

    update die method for mouse

--- mouse.js
+++ mouse.js
@@ -2,11 +2,16 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2023-03-13 23:47:42
- * @LastEditTime: 2023-03-13 23:49:32
+ * @LastEditTime: 2023-03-15 00:29:02
  * @LastEditors: Lewis
  */
 function mouse(name) {
   this.name - name;
+  this.isDead = false;
 }
+
+mouse.prototype.die = function () {
+  this.isDead = true;
+};
 
 module.export = mouse;
