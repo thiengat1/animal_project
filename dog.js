6d6fc9836a985d7304d8b9ebc94860627f0b0949
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-15 00:22:49 +0800

    add eat method for dog

--- dog.js
+++ dog.js
@@ -1,8 +1,15 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: Lewis
  * @Date: 2023-03-14 00:09:12
- * @LastEditTime: 2023-03-14 00:09:25
+ * @LastEditTime: 2023-03-15 00:22:17
  * @LastEditors: Lewis
  */
-function dog() {}
+function dog() {
+  this.stomach = [];
+}
+dog.prototype.eat = function (cat) {
+  this.stomach.push(cat);
+};
+
+module.export = dog;
