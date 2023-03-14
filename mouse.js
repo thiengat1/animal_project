85a40fc598b436e88ef86a4b4e5e56c8c35aad9a
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-15 00:27:05 +0800

    Revert "add die method"
    This reverts commit 5e8d00f6f8bf2e52f8c3b1b55abdc63a04bbaa9f.

--- mouse.js
+++ mouse.js
@@ -2,16 +2,11 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2023-03-13 23:47:42
- * @LastEditTime: 2023-03-13 23:59:02
+ * @LastEditTime: 2023-03-13 23:49:32
  * @LastEditors: Lewis
  */
 function mouse(name) {
   this.name - name;
-  this.dead = false;
 }
-
-mouse.prototype.die = function () {
-  this.dead = true;
-};
 
 module.export = mouse;
