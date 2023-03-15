cace8ffd208fd323c0bd19e06f15c5ae28da3b79
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-16 00:38:17 +0800

    add run method

--- horse.js
+++ horse.js
@@ -2,14 +2,17 @@
  * @Description:
  * @Author: Lewis
  * @Date: 2023-03-15 22:50:40
- * @LastEditTime: 2023-03-16 00:41:43
+ * @LastEditTime: 2023-03-16 01:04:41
  * @LastEditors: Lewis
  */
 function horse() {
   //
 }
 
-horse.prototype.jump=function(){
+horse.prototype.jump = function () {
   console.log('jumping');
-}
+};
+horse.prototype.run = function () {
+  console.log('running');
+};
 module.export = horse;
