940ef4e88db9ef51a0609c6172f22788d4b89e7a
Thien Ga <thienga@Thiens-MacBook-Pro.local>
2023-03-15 22:51:32 +0800

    add horse class

---
+++ horse.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @Author: Lewis
+ * @Date: 2023-03-15 22:50:40
+ * @LastEditTime: 2023-03-15 22:51:17
+ * @LastEditors: Lewis
+ */
+function horse() {}
+module.export = horse;
